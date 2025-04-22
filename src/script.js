a81c5e5d4e3c7a7eb31dc6c100db105c9a101ba9
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2025-04-22 14:28:58 -0300

    refatorando o header

--- src/script.js
+++ src/script.js
@@ -1,8 +1,8 @@
+let btn = document.querySelector("#dropd");
 let content = document.querySelector("#menu");
-
 let imgHamburguer = document.querySelector("#img1");
 
-function menu() {
+dropd.addEventListener("click", () => {
   content.classList.toggle("visible");
   if (content.classList.contains("visible")) {
     imgHamburguer.src = "/images/icon-close.svg";
@@ -10,24 +10,19 @@ function menu() {
     imgHamburguer.src = "/images/icon-hamburger.svg";
   }
   content.classList.toggle("hidden");
-}
-
-
-window.addEventListener("load", function () {
-  // wait until the page loads before working with HTML elements
-  document.addEventListener("click", function (event) {
-    //click listener on the document
-    document.querySelectorAll(".dropdown-content").forEach(function (el) {
-      if (el !== event.target) el.classList.remove("show");
-      // close any showing dropdown that isn't the one just clicked
-    });
-    if (event.target.matches(".dropbtn")) {
-      event.target
-        .closest(".dropdown")
-        .querySelector(".dropdown-content")
-        .classList.toggle("show");
-    }
-    // if this is a dropdown button being clicked, toggle the show class
-  });
 });
 
+document.addEventListener("click", function (event) {
+  //click listener on the document
+  document.querySelectorAll(".dropdown-content").forEach(function (el) {
+    if (el !== event.target) el.classList.remove("show");
+    // close any showing dropdown that isn't the one just clicked
+  });
+  if (event.target.matches(".dropbtn")) {
+    event.target
+      .closest(".dropdown")
+      .querySelector(".dropdown-content")
+      .classList.toggle("show");
+  }
+  // if this is a dropdown button being clicked, toggle the show class
+});
